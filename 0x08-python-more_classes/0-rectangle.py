653d8b2845b262fff6db99510983edb8a503c935
Moses Oyet David <trickmaxillar@gmail.com>
2022-01-16 04:05:07 +0300

    Modify

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-# Creating the class Rectangle
+"""
+Creates an empty class Rectangle
+"""
+
 
 class Rectangle:
-    """Initializing an empty class
-    using pass"""
-
+    """Initializing an empty classusing pass"""
     pass

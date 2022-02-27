741d10522a2fd280ece52b4d61694b369d77ae41
Moses Oyet David <trickmaxillar@gmail.com>
2022-02-27 10:28:38 +0300

    Initial commit

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- Print full description of the table first_table from the database hbtn_0c_0
+-- The database name will be passed as an argument of the mysql command
+-- DESCRIBE and EXPLAIN aren't used
+
+SHOW CREATE TABLE first_table;

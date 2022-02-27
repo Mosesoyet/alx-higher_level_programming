a545186ad3a08ea854fdca71bf472f8f9cbffbab
Moses Oyet David <trickmaxillar@gmail.com>
2022-02-27 10:45:31 +0300

    First commit

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+-- Displays the number of record with id = 89 in the table first_table
+-- of hbtn_0c_0
+-- The database name will be passed as an argument of the mysql command
+
+SELECT FROM first_table WHERE id IS 89;

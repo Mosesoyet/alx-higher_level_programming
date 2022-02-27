0ac296207a1ac46fe9982c3ba106f9140efcf029
Moses Oyet David <trickmaxillar@gmail.com>
2022-02-27 11:13:11 +0300

    First commit

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- List all records of second_table of hbtn_0c_0 database
+-- Result should display both the score and the name
+-- Records should order by score(top first)
+
+SELECT score, name FROM second_table ORDER BY score DESC;

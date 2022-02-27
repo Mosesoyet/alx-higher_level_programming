0320a537b9a0c87eeafd55f87c63be6a9109e2e1
Moses Oyet David <trickmaxillar@gmail.com>
2022-02-27 11:50:10 +0300

    First commit

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,8 @@
+-- List all records of the table second_table of the hbtn_0c_0
+-- Don't list rows without a name value
+-- Results should display the score and the name(in this order)
+-- Records should be listed by descending score
+
+SELECT score, name FROM second_table
+WHERE name EXISTS
+ORDER BY score DESC

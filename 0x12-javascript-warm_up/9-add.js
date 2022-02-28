84e82966a5b9972aa3e4a3ba114cd6b9d47ff72f
Moses Oyet David <trickmaxillar@gmail.com>
2022-02-28 16:56:07 +0300

    Fix

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 function add (a, b) {
-	let result = a + b;
-	console.log(result);
+  let result = a + b;
+  console.log(result);
 }
 
-add(number(process.argv[2]), number(process.argv[3]));
+add(Number(process.argv[2]), Number(process.argv[3]));

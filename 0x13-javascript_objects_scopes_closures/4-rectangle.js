65ee72e08d8a107c167d3b994bcff374d26c16e3
Moses Oyet David <trickmaxillar@gmail.com>
2022-03-01 11:54:50 +0300

    Modify

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+'use strict';
+class Rectangle {
+	constructor (w, h) {
+		if (((w = parseInt(w)) <= 0) && ((h = parseInt(h)) <= 0) {
+			this.width = w;
+			this.height = h;
+		}
+	}
+	print () {
+		console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.width));
+	}
+	rotate () {
+		let h = this.height;
+		let w = this.width;
+		this.width = h;
+		this.height = w;
+	}
+	double () {
+		this.height *= 2;
+		this.width *= 2;
+	}
+}
+module.exports = Rectangle;

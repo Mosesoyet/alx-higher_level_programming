9118c89f26116426ceab13f2c3b8332cb6aa6b45
Moses Oyet David <trickmaxillar@gmail.com>
2022-03-01 12:11:52 +0300

    Modify

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+'use strict';
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+	constructor (size) {
+		super(size, size);
+	}
+}
+
+module.exports = Square;

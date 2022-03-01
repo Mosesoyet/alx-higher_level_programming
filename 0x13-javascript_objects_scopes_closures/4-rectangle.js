b649d9cdc7d20922d2e7b3109b7ea66ba1f8506e
Moses Oyet David <trickmaxillar@gmail.com>
2022-03-01 12:05:14 +0300

    Modify

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,7 +2,7 @@
 'use strict';
 class Rectangle {
 	constructor (w, h) {
-		if (((w = parseInt(w)) <= 0) && ((h = parseInt(h)) <= 0) {
+		if (((w = parseInt(w)) > 0) && ((h = parseInt(h)) > 0) {
 			this.width = w;
 			this.height = h;
 		}

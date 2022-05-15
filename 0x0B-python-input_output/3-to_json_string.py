cced2f2bd26183aee7bd1ac2154f4cd0486bab1a
Moses Oyet David <trickmaxillar@gmail.com>
2022-05-15 12:21:42 +0300

    Use dumps() to serialize object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+A program that returns JSON representation of an object(string)
+"""
+
+
+def to_json_string(my_obj):
+    """A function that serializes my_obj to a json string
+    """
+    return json.dumps(my_obj)
